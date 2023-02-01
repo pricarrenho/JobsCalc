0c44f4782b7c4a213afa4223c382d54984b95397
Priscilla Carrenho <priscillaonil@gmail.com>
2023-02-01 14:26:03 -0300

    style: placed underline in the title

--- src/components/Title/styles.ts
+++ src/components/Title/styles.ts
@@ -4,5 +4,16 @@ export const Wrapper = styled.h1`
   ${({ theme }) => css`
     color: ${theme.colors.gray800};
     font-size: ${theme.font.sizes.xlg};
+    position: relative;
+
+    &::after {
+      content: "";
+      position: absolute;
+      width: 641px;
+      height: 0px;
+      border: 1.5px solid #e1e3e5;
+      bottom: 16px;
+      left: 0px;
+    }
   `}
 `;
