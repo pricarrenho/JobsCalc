6ba2c998bc8b3e86fb4a473f2ac1c89cfe552852
Priscilla Carrenho <priscillaonil@gmail.com>
2023-02-17 09:20:45 -0300

    fix: A bug fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,8 +9,8 @@ export const Routes = () => {
   return (
     <RouteLib>
       <Route path="/" element={<Home />} />
-      <Route path="/my-perfil" element={<MyProfile />} />
       <Route element={<ProtectedRoute />}>
+        <Route path="/my-perfil" element={<MyProfile />} />
         <Route path="edit-job/*" element={<EditJob />} />
         <Route path="add-new-job/*" element={<AddNewJob />} />
       </Route>
